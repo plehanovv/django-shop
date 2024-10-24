fa43f9f20ce257e410b7946902fcbd19144f65d3
yaroslav <plehanov.kzn2@gmail.com>
2024-10-24 16:28:50 +0300

    Setup models

--- django_shop/main/models.py
+++ django_shop/main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    slug = models.SlugField(max_length=20, unique=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [models.Index(fields=['name'])]
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
