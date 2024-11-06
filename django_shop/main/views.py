b1844bd5da54799070e85292e29e41d3330eb90f
yaroslav <plehanov.kzn2@gmail.com>
2024-11-06 19:19:35 +0300

    Add new html

--- django_shop/main/views.py
+++ django_shop/main/views.py
@@ -13,5 +13,11 @@ def product_detail(request, slug):
 
     return render(request, 'main/product/product.html', {'product': product})
 
-
-
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request, 'main/product/list.html', {'category': category, 'categories': categories, 'products': products})
